91d2eeced5bf80b1553f43009215174327d439ba
Muhammad Fatkhurrohman <hi@rohman.dev>
2023-07-30 22:20:26 +0700

    Updates

---
+++ _nuxt/404.1091a053.js
@@ -0,0 +1,1 @@
+import"./entry.f17399c7.js";const o=""+globalThis.__publicAssetsURL("404.svg");export{o as _};
--- _nuxt/404.f7d9a94a.js
+++
@@ -1,1 +0,0 @@
-import"./entry.d5e17550.js";const o=""+globalThis.__publicAssetsURL("404.svg");export{o as _};
